1b82a74bf82ebd901f797295f9ac2f6bb5ea3e00
aliia <aliya.beiwenalieva@gmail.com>
2025-10-24 21:06:50 +0600

    firday commit

---
+++ secondLesson.py
@@ -0,0 +1,23 @@
+# import math
+# a = math.pow(4,2)
+# print(a)
+# v =0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1
+# print(v)
+
+
+# myname = "Bob"
+# myName = "Alice"
+# my_name = "Eve"
+# sales_price = 19.99
+# print(5 >= 2)
+# >
+# <
+
+# a = "b"
+# a==b
+# ==
+
+
+
+
+print("Hello", 5, 333.555, False, 5<6)
---
+++ thirdLesson.py
@@ -0,0 +1,41 @@
+''' 
+
+print("hello, 'Sam'")
+
+dfdf
+dfdf
+dfdf
+dfdf
+dfd
+name = input("Enter your name: ")   #there i enter name
+sdfjhksdjflkms/ldkfm 
+# sdfkn.skdn.kv
+# '''
+# price  = int(input("Enter price: "))
+# print("Change is", 500-int(price))
+
+# name  = 'Tom'
+# name_int = int(name)
+# print(type(name_int))
+
+# print('hello', 344, 889.898, sep = '\n', end = '?')
+
+
+
+num = 5 / 2
+print(num)
+num2 = 5 // 2
+print(num2) 
+num3 = 110 % 10
+print(num3)
+num5 = 3 * 3
+print(num5)
+num6 = 3 ** 4
+print(num6)
+
+
+
+a = 5
+b = 2
+c = 10
+print(a * b / c)
