13ae71f3c296f8191511193c6eb875baf0182e21
aliia <aliya.beiwenalieva@gmail.com>
2025-10-29 15:04:18 +0600

    new commit

--- fourtLesson.py
+++ fourtLesson.py
@@ -1,25 +1,7 @@
-# isMarried = True
-# isWoman = True
 
-# name = "Alice"
-# age = 14
+num1 = -1
 
-
-# print("My name is", name, 'and I am', age, 'years old.')
-# print(f"My name is {name} and I am {age} years old.")
-
-# a = 100 / 25 + (25/100) / (100/25) - 10**2 + 100 ** .5
-# print(a)
-
-
-# a = [2, 3 5,545]
-# b = (2, 3, 5, 5)
-# c ={'key1': 'value1', 'key2': 'value2'}
-# a = 5
-# print(10 != 9)
-
-
-a = 5/3
-print(round(a))
-
+name = "Sam"
+name2 = 'Tom'
+print("my name is 'Alex'")
 
